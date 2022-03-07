8fdd3b307d1937f8080d8a16ed23e4aff40477b2
Kubsy <kubadd475@gmail.com>
2022-03-07 15:58:06 +0000

    Add code to show the player

--- game.py
+++ game.py
@@ -4,15 +4,23 @@ pygame.init()
 WINDOW = pygame.display.set_mode([500,500])
 RUNNING = True
 
+
+playerImg = pygame.image.load('assets/graphics/PNG/Player/Poses/Player_idle.png')
+playerImg.convert()
+playerRect = playerImg.get_rect()
+playerRect.center = 250, 250
+
+
 while RUNNING:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False 
 
-    #Draw the circle 
+    #Draw everything
     WINDOW.fill((255,255,255))
-    pygame.draw.circle(WINDOW, (0,0,255), (250,250), 75)
+    pygame.draw.circle(WINDOW, (0,0,255), (375,375), 75)
+    WINDOW.blit(playerImg, playerRect)
     pygame.display.flip()
 
 pygame.quit()

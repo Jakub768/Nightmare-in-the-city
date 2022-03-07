2f9490676785b5d26cc92bca48bad7744012aa23
Kubsy <kubadd475@gmail.com>
2022-03-07 15:29:54 +0000

    update comment

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False 
 
-    #Draw the circle by filling background, define circle colour which have 75 radius
+    #Draw the circle 
     WINDOW.fill((255,255,255))
     pygame.draw.circle(WINDOW, (0,0,255), (250,250), 75)
     pygame.display.flip()

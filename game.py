659249dc76e3d88f067e717bc6813af7ab7f3f0a
Kubsy <kubadd475@gmail.com>
2022-05-12 23:11:05 +0100

    update shooting

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 pygame.init()
+pygame.mixer.init()
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))) 
 MainClock = pygame.time.Clock()
@@ -56,7 +57,7 @@ def prepareGame():
     WINDOW.fill(WIN_COLOR)
     drawMainStreet()
     drawMiscellaneous()
-    drawBullet()
+    Shooting()
 
 def drawMainStreet():
     building = DrawBackground(0,400,200,700,(127,127,127))
@@ -71,22 +72,25 @@ def drawMainStreet():
 def drawMiscellaneous():
     person.drawPlayer(WINDOW)
 
-def drawBullet():
-    for bullet in BulletList:
+def Shooting():
+    for bullet in bulletList:
         bullet.drawBullet(WINDOW)
         bullet.shootBullet()
 
 #Define characters and other properties in the game
 person = Player(912,WINDOW_WIDTH/2,128,128)
-BulletList = []
+bulletList = []
 BULLET_TIME = 0.6
 timeSinceFire = 0
+
+pygame.mixer.music.load("Audio/No.mp3")
+pygame.mixer.music.play(0)
 
 while RUNNING:
 
     frameMs = MainClock.tick()
     frameSec = frameMs / 1000
-
+    
     keys = pygame.key.get_pressed()
 
     for event in pygame.event.get():
@@ -103,7 +107,7 @@ while RUNNING:
         person.y += person.vel * frameSec
 
     if keys[pygame.K_SPACE] and timeSinceFire >= BULLET_TIME:
-        BulletList.append(projectile(person.x+person.width//3,person.y+person.height//2,7,(255,255,0),6))
+        bulletList.append(projectile(person.x+person.width//3,person.y+person.height//2,7,(255,255,0),6))
         timeSinceFire = 0
 
     prepareGame()
